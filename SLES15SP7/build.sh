54ec07cfa266014185c6d2d6ba5ff32c0af01266
rkoktan <rkoktan@suse.com>
2026-02-27 11:11:56 -0500

    Update build.sh

--- SLES15SP7/build.sh
+++ SLES15SP7/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build . -t registry.suse.com/bci.bci-base:15.7-5.17.2 && docker push registry.suse.com/bci.bci-base:15.7-5.17.2
+docker build . -t labplex:5000/bci.bci-base:15.7-5.17.2 && docker push labplex:5000/bci.bci-base:15.7-5.17.2
